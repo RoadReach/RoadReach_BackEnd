0d31988dc29acd903ad81b837be8229314ee6ec9
Phani3458 <prasadch0706@gmail.com>
2025-09-11 14:13:29 -0500

    Upsert logic for country, state, city, airport, and vehicle data loaders

--- src/main/java/com/roadreach/roadreach_backend/repository/AirportRepository.java
+++ src/main/java/com/roadreach/roadreach_backend/repository/AirportRepository.java
@@ -3,5 +3,9 @@ package com.roadreach.roadreach_backend.repository;
 import com.roadreach.roadreach_backend.model.Airport;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.roadreach.roadreach_backend.model.State;
+import java.util.Optional;
+
 public interface AirportRepository extends JpaRepository<Airport, Long> {
+	Optional<Airport> findByCodeAndState(String code, State state);
 }
--- src/main/java/com/roadreach/roadreach_backend/repository/StateRepository.java
+++ src/main/java/com/roadreach/roadreach_backend/repository/StateRepository.java
@@ -3,5 +3,9 @@ package com.roadreach.roadreach_backend.repository;
 import com.roadreach.roadreach_backend.model.State;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.roadreach.roadreach_backend.model.Country;
+import java.util.Optional;
+
 public interface StateRepository extends JpaRepository<State, Long> {
+	Optional<State> findByAbbreviationAndCountry(String abbreviation, Country country);
 }
